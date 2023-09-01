da45997e811f45074a061888aa9efe432d61a098
olivia <oliviaw2026@student.cis.edu.hk>
2023-09-01 09:31:10 +0800

    Added main method and print statement.

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);				// 1
+        System.out.println("Enter your age");	// 2
+        int age = Integer.parseInt(scanner.nextLine());					// 3
+        System.out.println(age+ "-year olds should date somebody who is at least " + (7+(age/2)) + " years old.");
     }
 }
