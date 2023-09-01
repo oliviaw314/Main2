43dafc19b8c50d3441106adc6dd4c283cc1ed11d
olivia <oliviaw2026@student.cis.edu.hk>
2023-09-01 14:14:29 +0800

    Added main method and print statement.

--- src/word_lastindex.java
+++ src/word_lastindex.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class word_lastindex {
     public static void main(String[]args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter a word");
-    String input = scanner.nextLine();
-    int LastIndex = input.length()-1;
-    System.out.println("The index of the last letter is "+LastIndex+".");
-    double castToDouble = Double.parseDouble(String.valueOf(LastIndex));
-    System.out.println("Converted to double: "+castToDouble);
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter a word");
+            String input = scanner.nextLine();
+            int LastIndex = input.length() - 1;
+            System.out.println("The index of the last letter is " + LastIndex + ".");
+            double castToDouble = Double.parseDouble(String.valueOf(LastIndex));
+            System.out.println("Converted to double: " + castToDouble);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid number!");
+        }
     }
 }
